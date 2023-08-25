565c66e083a873bc409dfcf18fe642a5ea3c75e4
mohammed chaouki <crazynomadd@192.168.1.2>
2023-08-25 04:28:50 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+// App
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  function addItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Header />;
+      <Form onAddItems={addItems} />
+      <PackingList
+        items={items}
+        onDelete={handleDeleteItems}
+        onToggle={handleToggleItem}
+        key={items.id}
+      />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Travel list</h1>
+    </header>
+  );
+}
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleForm(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
+
+    onAddItems(newItem);
+  }
+
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleForm}>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
         >
-          Learn React
-        </a>
-      </header>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((n) => (
+            <option value={n} key={n}>
+              {n}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="item ..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+function PackingList({ items, onDelete, onToggle }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDelete={onDelete}
+            onToggle={onToggle}
+            key={item.key}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item, onDelete, onToggle }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggle(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDelete(item.id)}>&times;</button>
+    </li>
+  );
+}
